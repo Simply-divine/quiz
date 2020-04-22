d11661a6551a57da1b400bbba8b2b5dcf6dd71b7
c2oo13 <smitpat2000@gmail.com>
2020-04-23 00:19:52 +0530

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "written", to: "users#written"
+
+  resources :users
+  resources :questions do
+    resources :options
+  end
+  resources :user_answers
+
 end
