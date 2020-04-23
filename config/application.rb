8ac7fe7680bf6ce2ebc72ebce803cb0f7927b874
Hardik <hardikvu0204@gmail.com>
2020-04-23 17:16:31 +0530

    precompiling assets solve

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Quiz
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-		config.assets.initialize_on_precompile = false
+		config.assets.compile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
