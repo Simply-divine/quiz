973d368d361daa6898c5d89cf8ef30197233236b
Hardik <hardikvu0204@gmail.com>
2020-04-23 20:24:04 +0530

    changed to user_quizzes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true, length: {minimum: 3, maximum: 100}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 5, maximum: 100}
-  has_many :user_quizs
-  has_many :subgenres, :through => :user_quizs
+  has_many :user_quizzes
+  has_many :subgenres, :through => :user_quizzes
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_053130) do
+ActiveRecord::Schema.define(version: 2020_04_23_144526) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
