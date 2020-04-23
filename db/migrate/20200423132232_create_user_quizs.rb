d0904a38594f74c2482289c772c706b29a1dc0cc
Hardik <hardikvu0204@gmail.com>
2020-04-23 18:55:31 +0530

    fdfs

--- db/migrate/20200423132232_create_user_quizs.rb
+++ db/migrate/20200423132232_create_user_quizs.rb
@@ -1,6 +1,6 @@
-class CreateUserQuizzes < ActiveRecord::Migration[6.0]
+class CreateUserQuizs < ActiveRecord::Migration[6.0]
   def change
-    create_table :user_quizzes do |t|
+    create_table :user_quizs do |t|
       t.references :user, foreign_key: true
       t.references :subgenre, foreign_key: true
       t.integer :score
