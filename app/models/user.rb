3b7f71af6b7c042b1c3208fed23d8f45fe4f4835
c2oo13 <smitpat2000@gmail.com>
2020-04-23 08:29:15 +0530

    Changed migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 						length: {maximum: 105},
 						uniqueness: {case_sensitive: false},
 						format: { with: VALID_EMAIL_REGEX }
-  has_many :user_answer, dependent: :destroy, inverse_of: :user
-  accepts_nested_attributes_for :user_answer
+  has_many :useranswer, dependent: :destroy, inverse_of: :user
+  accepts_nested_attributes_for :useranswer
 	has_secure_password
 end
