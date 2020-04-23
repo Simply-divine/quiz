a41eec48eb4247f02a6c2338de4475a21503c417
c2oo13 <smitpat2000@gmail.com>
2020-04-23 10:32:13 +0530

    12

--- app/models/useranswer.rb
+++
@@ -1,6 +0,0 @@
-class Useranswer < ApplicationRecord
-
-  belongs_to :user
-  belongs_to :question
-
-end
---
+++ db/migrate/20200423045708_create_questions.rb
@@ -0,0 +1,17 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.references :subgenre, foreign_key: true
+      t.string :question
+      t.string :option1
+      t.string :option2
+      t.string :option3
+      t.string :option4
+      t.boolean :check1
+      t.boolean :check2
+      t.boolean :check3
+      t.boolean :check4
+      t.timestamps
+    end
+  end
+end
